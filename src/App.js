7cb776e27a08743ab3654e48fb80f5096d92291b
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-05-30 16:28:14 -0400

    Semantic added

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ function App() {
   return (
     <Router>
       <div>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/about">
             <About />
