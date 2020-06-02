b630d888f42f1cdca89d225c96ec03639f16e947
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-06-02 16:12:15 -0400

    Using mars environmental variables

--- src/everglade/common/util/loginAuth.js
+++ src/everglade/common/util/loginAuth.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
+import runtimeEnv from '@mars/heroku-js-runtime-env'
 
 function authorizeLogin(username, password) {
+
+    const env = runtimeEnv();
 
     return {
         pog: process.env.API_KEY,
         pog2: process.env.apiKey,
         pog3: process.env,
-        pog4: "Pog"
+        pog4: "Pog",
+        unpog: env.API_KEY,
+        unpog2: env.apiKey,
+        unpog3: env,
+        unpog4: "Unpog"
     }
 }
 
