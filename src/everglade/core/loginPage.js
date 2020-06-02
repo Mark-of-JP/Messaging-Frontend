04a4e31b8d2aa32f858e637747afabff30d0cebb
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-06-02 02:26:16 -0400

    Added login basics ui

--- src/everglade/core/loginPage.js
+++ src/everglade/core/loginPage.js
@@ -1,10 +1,52 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Segment, Form, Container, Grid, Button, Divider, Header, Menu } from 'semantic-ui-react'
 
 class LoginPage extends Component {
 
-    render(){
+    render() {
         return (
-            <h1>Eat Login Nut</h1>
+            <div>
+                <Segment
+                inverted
+                textAlign='center'
+                style={{ padding: '0.5em 4em' }}
+                vertical
+                raised>
+                    <Menu
+                    fixed='top'
+                    inverted
+                    pointing
+                    secondary={false}
+                    size='large'>
+                        <Menu.Item position='right'>
+                            <Header as='h4' style={{color:'white'}}>Don't have an account?</Header>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button as ='a' inverted primary={false}>Sign Up</Button>
+                        </Menu.Item>
+                    </Menu>
+                </Segment>
+
+                <Container style={{
+                    position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: '500px',
+                }}>
+                    <Form>
+                        <Form.Input
+                            label='Email'
+                            placeholder='Email'
+                        />
+                        <Form.Input
+                            label='Password'
+                            placeholder='Password'
+                            type='password'
+                        />
+                        <Form.Checkbox label='Remember Me'/>
+                        <Form.Button>Login</Form.Button>
+                    </Form>
+                </Container>
+            </div>
         )
     }
 }
