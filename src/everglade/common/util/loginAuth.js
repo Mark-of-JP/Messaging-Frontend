7db7cec550c0489c9587a004573d4dd720c5647b
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-06-02 15:53:03 -0400

    Gimme environment variables

--- src/everglade/common/util/loginAuth.js
+++ src/everglade/common/util/loginAuth.js
@@ -2,7 +2,12 @@ import {} from 'dotenv/config'
 
 function authorizeLogin(username, password) {
 
-    return process.env.API_KEY
+    return {
+        pog: process.env.API_KEY,
+        pog2: process.env.apiKey,
+        pog3: process.env,
+        pog4: "Pog"
+    }
 }
 
 export default authorizeLogin;
