27e9236e4a167d12f3e7b0658235bf478f21e1c9
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-06-12 17:56:56 -0400

    Added Sidebar search

--- src/everglade/common/components/messagingSideBar.js
+++ src/everglade/common/components/messagingSideBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { List, Image } from 'semantic-ui-react'
+import { List, Image, Menu, Dropdown, Input } from 'semantic-ui-react'
 import PerfectScrollbar from 'react-perfect-scrollbar'
 
 import 'react-perfect-scrollbar/dist/css/styles.css'
@@ -9,11 +9,11 @@ class MessagingSideBar extends Component {
     generateListContent = (num) => {
         var content = []
 
-        for(var i = 0; i < num; i++) {
+        for (var i = 0; i < num; i++) {
             content.push(<List.Item>
                 <Image avatar placeholder />
                 <List.Content>
-            <List.Header>Friend #{i + 1}</List.Header>
+                    <List.Header>Friend #{i + 1}</List.Header>
                 </List.Content>
             </List.Item>)
         }
@@ -23,15 +23,35 @@ class MessagingSideBar extends Component {
 
     render() {
         return (
-            <div style={{ flex: 1, display: 'flex', height: '100%', flexDirection: 'column' }}>
-                <div style={{ flex: 1, backgroundColor:'green' }} />
-                <PerfectScrollbar style={{ flex: 12, height: '100%', backgroundColor:'#1B1C1D' }}>
+            <Menu inverted fixed='left' vertical style={{ display: 'flex', flexDirection: 'column' }} size='large'>
+                <Menu.Item>
+                    <Dropdown text="Friends"
+                        inline
+                        fluid
+                        style={{ flex: 1 }}
+                    >
+                        <Dropdown.Menu>
+                            <Dropdown.Item text="Friends" />
+                            <Dropdown.Item text="Chats" />
+                            <Dropdown.Item text="Settings" />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
+
+                <Menu.Item>
+                    <Input placeholder='Search...' icon='search' />
+                </Menu.Item>
+
+                <PerfectScrollbar style={{ flex: 12, height: '100%', backgroundColor: '#1B1C1D' }}>
                     <List inverted selection verticalAlign='middle'>
                         {this.generateListContent(30)}
                     </List>
                 </PerfectScrollbar>
-                <div style={{ flex: 1, backgroundColor: 'purple' }} />
-            </div>
+
+                <Menu.Item>
+                    <div style={{ flex: 1, backgroundColor: 'purple' }} />
+                </Menu.Item>
+            </Menu>
         )
     }
 
