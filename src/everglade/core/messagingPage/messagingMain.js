1ab264b011f1b865bbc13d48848103eeb5d7154f
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-06-13 17:14:00 -0400

    Added message section

--- src/everglade/core/messagingPage/messagingMain.js
+++ src/everglade/core/messagingPage/messagingMain.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react'
-import { TextArea, Form } from 'semantic-ui-react'
+import { TextArea, Form, Divider, Comment } from 'semantic-ui-react'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+
+function generateMessage(author, message) {
+    return (
+        <Comment>
+            <Comment.Avatar />
+            <Comment.Content>
+                <Comment.Author as='a'>{author}</Comment.Author>
+                <Comment.Metadata>
+                    <div>Today at 4:20AM</div>
+                </Comment.Metadata>
+                <Comment.Text>{message}</Comment.Text>
+            </Comment.Content>
+        </Comment>
+    )
+}
 
 class MessagingMain extends Component {
 
     render() {
         return (
-            <div style={{display:'flex', flex: 4, flexDirection:'column'}}>
+            <div style={{ display: 'flex', flex: 4, flexDirection: 'column' }}>
 
-                <div style={{ flex: 1}}>
-
-                </div>
-
-                <div style={{ flex: 6}}>
+                <div style={{ flex: 1 }}>
 
                 </div>
 
-                <Form style={{ flex: 1, padding:'1em 2em' }}>
-                    <TextArea placeholder='Send a message...' style={{resize:'none'}}/>
+                <PerfectScrollbar style={{ maxHeight: '100%', marginRight: '15px', flex: 6, backgroundColor: '#424547' }}>
+                    <div style={{ display: 'flex', alignItems: 'flex-end', minHeight:'100%' }}>
+                        <Comment.Group>
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('Bean', 'BEANZ')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+                            {generateMessage('SoftBoiUwu', 'Eat Nut 12')}
+
+
+                        </Comment.Group>
+                    </div>
+                </PerfectScrollbar>
+
+
+                <div style={{ flex: 0.1, position: 'relative' }}>
+                    <Divider inverted />
+                </div>
+
+                <Form style={{ flex: 1, padding: '1em 2em' }}>
+                    <TextArea placeholder='Send a message...' style={{ resize: 'none' }} />
                 </Form>
             </div>
         )
