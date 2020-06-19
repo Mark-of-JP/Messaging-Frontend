c7a1a1f9317aae343b99bbbbb37b906f6def7e9a
EverGladeZ <60991984+EverGladeZ@users.noreply.github.com>
2020-06-19 18:05:56 -0400

    Fixed spelling errors and grammar

--- src/everglade/core/homepage/homepageAbout.js
+++ src/everglade/core/homepage/homepageAbout.js
@@ -56,14 +56,14 @@ class HomepageAbout extends Component {
 
                 <Container style={{padding:'3em 0em'}}>
                     <p style={{ fontSize: '1.33em' }}>
-                        Everglade Messaging is an online messaging web app which was created and design as a side project. Both the backend and the frontend of the applcation were created by the 3 developers.
-                        The technology that was used are as follows:
+                        Everglade Messaging is an online messaging web app which was created and designed as a side project. Both the backend and the frontend of the application were created by three developers.
+                        The technology that was used is as follows:
                     </p>
                 </Container>
 
-                {this.generateTechnologyInfo(ReactLogo, 'React.js', 'React is a popular frontend framework developed by Facebook. React is used for the frontend of our application and it is what is currently being used to display this page.', false)}
-                {this.generateTechnologyInfo(HerokuLogo, 'Heroku', 'Heroku is a platform as a service that allows developers to develop and deploy their online web apps. Heroku is being used to host the React frontend of our application.', true)}
-                {this.generateTechnologyInfo(FlaskLogo, 'Flask', 'Flask is a python web application framework that is being used for the backend of our application. We are using flask for our REST API as well as for our websockets.', false)}
+                {this.generateTechnologyInfo(ReactLogo, 'React.js', 'React is a popular frontend framework developed by Facebook. React is used for the frontend of our application and is currently being used to display this page.', false)}
+                {this.generateTechnologyInfo(HerokuLogo, 'Heroku', 'Heroku is a platform that allows developers to develop and deploy their online web apps. Heroku is being used to host the React frontend of our application.', true)}
+                {this.generateTechnologyInfo(FlaskLogo, 'Flask', 'Flask is a python web application framework that is being used for the backend of our application. We are using Flask for our REST API as well as for our websockets.', false)}
                 {this.generateTechnologyInfo(FirebaseLogo, 'Google Firebase', "Firebase is Google's mobile platform. It is hosting our python web app as well as being our database. Authentication is also being handled by Firebase.", true)}
             </div>
         )
