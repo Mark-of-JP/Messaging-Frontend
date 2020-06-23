99644771f63e681ddc1a9c12ff181995bef9284f
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-06-23 12:50:12 -0400

    Added local comment

--- src/everglade/core/websocketTest/index.js
+++ src/everglade/core/websocketTest/index.js
@@ -9,6 +9,7 @@ class WebsocketTest extends Component {
         super(props)
 
         socket = SocketIOClient('everglade-messaging.web.app')
+        //socket = SocketIOClient('localhost:5000')
         socket.on('connect', () => console.log('poggers'))
         socket.on('pog', pog => console.log(pog))
     }
