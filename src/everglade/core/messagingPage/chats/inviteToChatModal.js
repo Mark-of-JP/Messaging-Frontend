21ce4445b18f7c26938b5f7e98a19a7745b231f4
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-07-30 18:15:41 -0400

    Added invite list

--- src/everglade/core/messagingPage/chats/inviteToChatModal.js
+++ src/everglade/core/messagingPage/chats/inviteToChatModal.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
-import { Modal, Button, Icon } from 'semantic-ui-react'
+import { Modal, Button, Icon, Input, List, Image } from 'semantic-ui-react'
 
 class InviteModal extends Component {
     render() {
         return (
             <Modal centered={false} trigger={(
-                <Button style={{ alignSelf: 'center',  marginRight: '1em' }} icon labelPosition='right' color='green'>
-                        <Icon name="user plus" />
+                <Button style={{ alignSelf: 'center', marginRight: '1em' }} icon labelPosition='right' color='green'>
+                    <Icon name="user plus" />
                         Invite User
-                    </Button>
+                </Button>
             )}>
                 <Modal.Header>Invite Friend</Modal.Header>
                 <Modal.Content>
                     <Modal.Description>
+                        <Input placeholder='Search...' icon='search' fluid />
+                        <List celled selection>
+                            <List.Item style={{ display: 'flex' }} >
+                                <Image avatar placeholder />
+                                <List.Content style={{ flex: 1, alignSelf: 'center', }}>
+                                    <List.Header>Pog 1</List.Header>
+                                </List.Content>
+                            </List.Item>
+                            <List.Item >
+                                <Image avatar placeholder />
+                                <List.Content>
+                                    <List.Header>Pog 2</List.Header>
+                                </List.Content>
+                            </List.Item>
+                            <List.Item >
+                                <Image avatar placeholder />
+                                <List.Content>
+                                    <List.Header>Pog 1</List.Header>
+                                </List.Content>
+                            </List.Item>
+                        </List>
                     </Modal.Description>
                 </Modal.Content>
             </Modal>
