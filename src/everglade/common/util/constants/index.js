133895a00a14a59fc8ec9e271132689f1ae17c0e
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-08-03 20:43:49 -0400

    Fixed url

--- src/everglade/common/util/constants/index.js
+++ src/everglade/common/util/constants/index.js
@@ -1,3 +1,3 @@
 require('dotenv').config()
 
-export const apiUrl = process.env.REACT_APP_API_URL !== undefined ? process.env.REACT_APP_API_URL : 'http://localhost:5000/'
+export const apiUrl = process.env.REACT_APP_API_URL !== undefined ? process.env.REACT_APP_API_URL : 'https://everglade-messaging.web.app/'
